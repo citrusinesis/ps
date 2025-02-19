a70aa687b61872d2db95907c26984fd984286087
Jiho Song <jihojiho2003@gmail.com>
2025-02-19 16:54:46 +0900

    baekjoon: 1149

--- problems/baekjoon/1149/main.go
+++ problems/baekjoon/1149/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -14,103 +13,42 @@ var (
 	writer  = bufio.NewWriter(os.Stdout)
 )
 
-type Color uint
-
 const (
-	Red Color = iota
+	Red = iota
 	Green
 	Blue
 )
 
-type PriceMap [][3]int
-
-func NewPriceMap(s *bufio.Scanner) PriceMap {
-	var priceMap PriceMap
-	var length int
-	if s.Scan() {
-		length, _ = strconv.Atoi(s.Text())
-		priceMap = make(PriceMap, length)
+func min(a, b int) int {
+	if a < b {
+		return a
 	}
-
-	for i := 0; i < length; i++ {
-		priceMap[i] = [3]int{}
-		if s.Scan() {
-			for j, price := range strings.Fields(s.Text()) {
-				priceMap[i][j], _ = strconv.Atoi(price)
-			}
-		}
-	}
-
-	return priceMap
-}
-
-type ColorNode struct {
-	color        Color
-	price, depth int
-	left, right  *ColorNode
-}
-
-func NewColorNode(color Color, price, depth int) *ColorNode {
-	return &ColorNode{
-		color: color,
-		price: price,
-		depth: depth,
-	}
-}
-
-func buildColorTree(root *ColorNode, priceMap PriceMap) {
-	if root.depth == len(priceMap)-1 {
-		return
-	}
-
-	nextDepth := root.depth + 1
-	switch root.color {
-	case Red:
-		root.left = NewColorNode(Green, root.price+priceMap[nextDepth][Green], nextDepth)
-		root.right = NewColorNode(Blue, root.price+priceMap[nextDepth][Blue], nextDepth)
-	case Green:
-		root.left = NewColorNode(Red, root.price+priceMap[nextDepth][Red], nextDepth)
-		root.right = NewColorNode(Blue, root.price+priceMap[nextDepth][Blue], nextDepth)
-	case Blue:
-		root.left = NewColorNode(Red, root.price+priceMap[nextDepth][Red], nextDepth)
-		root.right = NewColorNode(Green, root.price+priceMap[nextDepth][Green], nextDepth)
-	}
-
-	buildColorTree(root.left, priceMap)
-	buildColorTree(root.right, priceMap)
-}
-
-func findLowestPrice(root *ColorNode) int {
-	if root.left == nil && root.right == nil {
-		return root.price
-	}
-
-	left := findLowestPrice(root.left)
-	right := findLowestPrice(root.right)
-
-	if left < right {
-		return left
-	}
-	return right
+	return b
 }
 
 func main() {
 	defer writer.Flush()
-	priceMap := NewPriceMap(scanner)
 
-	trees := [3]*ColorNode{
-		NewColorNode(Red, priceMap[0][Red], 0),
-		NewColorNode(Green, priceMap[0][Green], 0),
-		NewColorNode(Blue, priceMap[0][Blue], 0),
-	}
-
-	lowest := math.MaxInt
-	for _, tree := range trees {
-		buildColorTree(tree, priceMap)
-		if price := findLowestPrice(tree); lowest > price {
-			lowest = price
+	scanner.Scan()
+	length, _ := strconv.Atoi(scanner.Text())
+	costs := make([][3]int, length)
+	for i := 0; i < length; i++ {
+		scanner.Scan()
+		fields := strings.Fields(scanner.Text())
+		for j := 0; j < 3; j++ {
+			costs[i][j], _ = strconv.Atoi(fields[j])
 		}
 	}
 
-	fmt.Fprintln(writer, lowest)
+	dp := make([][3]int, length)
+	dp[0] = costs[0]
+
+	for depth := 1; depth < length; depth++ {
+		dp[depth][Red] = costs[depth][Red] + min(dp[depth-1][Green], dp[depth-1][Blue])
+		dp[depth][Green] = costs[depth][Green] + min(dp[depth-1][Red], dp[depth-1][Blue])
+		dp[depth][Blue] = costs[depth][Blue] + min(dp[depth-1][Red], dp[depth-1][Green])
+	}
+
+	result := min(dp[length-1][Red], min(dp[length-1][Green], dp[length-1][Blue]))
+	fmt.Fprintln(writer, result)
 }
