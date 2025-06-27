4c885b8cd8502d80690757494ab722bad1961ee8
Jiho Song <jihojiho2003@gmail.com>
2025-06-27 15:43:26 +0900

    baekjoon: 2839

--- baekjoon/2839.cc
+++ baekjoon/2839.cc
@@ -1,26 +1,21 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 int main() {
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
 
-    int N;
-    std::cin >> N;
+  int N;
+  std::cin >> N;
 
-    std::vector<int> dp(N / 5, -1);
-    for (int i = 0; i <= (N / 5); i++) {
-        const int r = N - (i * 5);
-        if (r % 3 == 0) {
-            if (i == 0) {
-                dp[i] = i + r / 3;
-            }
-            dp[i] = dp[i - 1] != -1 ? std::min(i + r / 3, dp[i - 1]) : i + r / 3;
-        }
-        dp[i] = i == 0 ? -1 : dp[i - 1];
+  int ans = -1;
+
+  for (int i = N / 5; i >= 0; --i) {
+    if (const int r = N - (i * 5); r % 3 == 0) {
+      ans = i + r / 3;
+      break;
     }
+  }
 
-    std::cout << dp.back() << '\n';
-
-    return 0;
+  std::cout << ans << '\n';
+  return 0;
 }
