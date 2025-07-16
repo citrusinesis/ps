85c09dc64984e267705c05eab28b82069bf9be7b
Jiho Song <jihojiho2003@gmail.com>
2025-07-16 13:44:52 +0900

    baekjoon: 15552

---
+++ baekjoon/15552.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int T;
+  std::cin >> T;
+
+  while (T--) {
+    int A, B;
+    std::cin >> A >> B;
+    std::cout << A + B << '\n';
+  }
+
+  return 0;
+}
